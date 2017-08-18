b3c764d0f73c6fa3ae43f3a600939c4a2ab320f4
Imani Oliver <isaac@ihardy.co>
2017-08-18 09:21:56 -0400

    everything works

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 
 let user = {username: "imani.oliver@gmail.com", password: "password"};
+let messages = [];
+let obj;
 // let token = "a123456798765z";
 
 function authenticate(req, res, next) {
@@ -16,29 +18,35 @@ function authenticate(req, res, next) {
 };
 
 router.get('/login', function(req, res){
-    res.render('login');
+    if(req.session.token){
+        res.redirect('/')
+    } else {
+        res.render('login', {errors:obj});
+    }
 });
 
 router.get('/', authenticate, function(req,res) {
-            res.render('results', {user:user})
+    res.render('results', {user:user});
     });
 
 router.post('/', function(req, res){
 
+    messages = [];
+    
         req.checkBody("email", "must be a valid email").isEmail();
         req.checkBody("email", "can't be empty").notEmpty();
         req.checkBody("password", "Password must contain at least 8 characters").notEmpty().isLength({min:8});
         req.checkBody("username", "Username must be at least 8 characters and no more than 25 characters, and may only use numbers and letters.").notEmpty().isLength({min:8}).isLength({max:25}).isWhitelisted('', 'abcd');
 
         let errors = req.getValidationResult();
-        let messages = [];
+
 
         errors.then(function(result){
             result.array().forEach(function(error){
                 messages.push(error.msg);
             });
 
-            let obj = {
+            obj = {
               errors: messages,
               username: req.body.username,
               email: req.body.email,
@@ -50,40 +58,42 @@ router.post('/', function(req, res){
 
 //above are the errors. where do I render?
 
-res.render('/', obj);
-//
-//
-//     let object = {
-//         username: req.body.username,
-//         password: req.body.password
-//     }
-//
-//     console.log(object);
-//     console.log(user);
-//
-//
-//
-//
-//     if (user.username==object.username && user.password==object.password) {
-//         console.log("matching login");
-//         req.session.user = user;
-//         req.session.token = "a123456798765z";
-//         res.redirect('/');
-//     } else {
-//         console.log("redirected");
-//         res.redirect("/login")
-//     }
-//
-// });
-//
-//
-// router.get("/logout", function(req, res) {
-//   // req.session.destroy(); is good too
-//   req.session.destroy(function(err) {
-//     console.log(err);
+// res.render('/', )
+
+
+    let object = {
+        username: req.body.username,
+        password: req.body.password
+    }
+
+    console.log(object);
+    console.log(user);
+
+
+
+
+    if (user.username==object.username && user.password==object.password) {
+        console.log("matching login");
+        req.session.user = user;
+        req.session.token = "a123456798765z";
+        res.redirect('/');
+    } else {
+        console.log("redirected");
+        res.redirect("/login")
+    }
+
+ });
+});
+
+
+
+router.get("/logout", function(req, res) {
+  // req.session.destroy(); is good too
+  req.session.destroy(function(err) {
+    console.log(err);
   });
-//
-//   res.redirect("/login");
+
+  res.redirect("/login");
 });
 
 
